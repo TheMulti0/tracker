4986cd19065d3f299eb3f7011bf97a8d598e597f
TheMulti0 <multi@codeprecise.com>
2022-09-23 22:31:27 +0300

    Location tracking

---
+++ DesktopServer/WebApplication2/Controllers/LocationUpdate.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Controllers ;
+
+    public record LocationUpdate
+    {
+        public double Lat { get; init; }
+        public double Lon { get; init; }
+        public double Alt { get; init; }
+    }
--- client/app/src/main/java/themulti0/tracker/client/MainActivity.java
+++ client/app/src/main/java/themulti0/tracker/client/MainActivity.java
@@ -1,51 +1,71 @@
 package themulti0.tracker.client;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
-import java.io.BufferedInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.URL;
-
+import themulti0.tracker.client.http.LocationUpdate;
 import themulti0.tracker.client.http.RequestFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestFactory requestFactory = new RequestFactory();
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        startLocationUpdates();
 
-        RequestFactory f = new RequestFactory();
+        queue = Volley.newRequestQueue(this);
+    }
 
-        Request<Boolean> post = f.post(
-                "login",
-                "",
-                response -> {
-                    System.out.println(response);
-                },
-                r -> {
+    private void startLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(1000);
 
-                }
-        );
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+            }
 
-        queue.add(post);
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Location lastLocation = locationResult.getLastLocation();
 
+                queue.add(requestFactory.post(
+                    "location",
+                    new LocationUpdate(lastLocation.getLatitude(), lastLocation.getLongitude(), lastLocation.getAltitude()),
+                    e -> {},
+                    e -> {}
+                ));
+            }
+        };
+
+        LocationServices.getFusedLocationProviderClient(this)
+            .requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
     }
 }
